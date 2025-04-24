5d7bc0dcc0973c4bbfc6535f34a5d23083f51c60
Lucas Dias Developer <devweblucasdias@gmail.com>
2025-04-24 05:27:40 -0300

    header - 1 etapa implementada

---
+++ src/components/Header/header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./header.css";
+import { FaSearch } from "react-icons/fa";
+import logo from "../../assets/logo-1.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <img src={logo} alt="Logo RP Gás" className="logo" />
+
+        <div className="header-right">
+          <span>FALE PELO WHATSAPP</span>
+          <span>0800 0100 041</span>
+          <FaSearch className="search-icon" />
+        </div>
+      </div>
+      <hr className="header-line" />
+    </header>
+  );
+};
+
+export default Header;
